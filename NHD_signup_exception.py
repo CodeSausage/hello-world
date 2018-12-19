d5273c9765c0de98d9ee03ea613d81a2c255e4aa
handsomeGRC <34636378+handsomeGRC@users.noreply.github.com>
2018-12-19 10:37:23 +0800

    delete some comments
    as the title, just delete some comments.

--- NHD_signup_exception.py
+++ NHD_signup_exception.py
@@ -41,21 +41,3 @@ if On_Internet:
 		Go to do what you should do!")
 # finally:
 # 	driver.close()
-
-# elem.send_keys(Keys.RETURN)
-# assert "No results found." not in driver.page_source
-# driver.close()
-# 
-# driver.get("https://morvanzhou.github.io/")
-# driver.get("https://www.cc98.org/")
-# driver.find_element_by_xpath(u"//img[@alt='强化学习 (Reinforcement Learning)']").click()
-# driver.find_element_by_link_text("About").click()
-# driver.find_element_by_link_text(u"赞助").click()
-# driver.find_element_by_link_text(u"教程 ▾").click()
-# driver.find_element_by_link_text(u"数据处理 ▾").click()
-# driver.find_element_by_link_text(u"网页爬虫").click()
-
-
-# html = driver.page_source       # get html
-# driver.get_screenshot_as_file("./img/sreenshot1.png")
-# driver.close()
